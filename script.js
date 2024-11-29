c1c90d92320e57832bc92cc336a24adf5d987228
Mr.exe <49715949+andrew7str@users.noreply.github.com>
2024-11-30 01:25:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+document.getElementById('start-button').addEventListener('click', () => {
+    alert("Start menu clicked!");
+});
 
+// You can add more functionality here, like opening windows or adding drag-and-drop functionality.
